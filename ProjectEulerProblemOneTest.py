7e529b9b798d1d693f621c3966784ba741059f70
alecdadey <98932264+alecdadey@users.noreply.github.com>
2022-02-02 21:29:51 -0600

    Add files via upload

---
+++ ProjectEulerProblemOneTest.py
@@ -0,0 +1,16 @@
+def main():
+    num = int(input('Insert number: ')) #takes an integer as input
+    output = sumOfMultiples(num) 
+    print(output)
+
+
+def sumOfMultiples(param):
+    j = 0
+    i = 0
+    for i in range(i, param):
+        if (i % 3 ==0) or (i % 5 == 0) and (i % 15 != 0):
+            j = j + i
+    return j
+
+if __name__ == '__main__':
+    main()
